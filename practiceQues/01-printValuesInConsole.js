a63c4d2dd75d988b4d54d8c38484d7d8f415054d
vishnu2815 <vishnumathariya28@gmail.com>
2026-05-20 22:29:46 +0530

    Today's work

---
+++ practiceQues/01-printValuesInConsole.js
@@ -0,0 +1,10 @@
+const yourName = "Vishnu"
+
+const yourAge = 25
+
+const yourCity = "Pune"
+
+console.log(`Name: ${yourName}`);
+console.log(`Age: ${yourAge}`);
+console.log(`City: ${yourCity}`);
+
---
+++ practiceQues/02-printProducts.js
@@ -0,0 +1,9 @@
+const productName = String(prompt("Enter product name "))
+const productPrice = Number(prompt("Enter product price"))
+const isAvailable = prompt("Is product available ?true/false" ) ==="true"
+
+
+console.log(`Product: ${productName}`);
+console.log(`Price: ${productPrice}`);
+console.log(`Available: ${isAvailable}`);
+
